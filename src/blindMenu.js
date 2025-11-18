ccbdcc47e93b5c249dfbe8b9bdf77eba4e0e0024
lih7912 <lih7912@autostock.co.kr>
2025-11-18 14:35:32 +0900

    brandon 추가

--- src/blindMenu.js
+++ src/blindMenu.js
@@ -79,7 +79,7 @@ function setShow(menu) {
 async function blindMenu(window, apolloOption, userInfoForAuth) {
     let userId = userInfoForAuth.userId;
     
-    if (userId === 'lih7912' || userId === 'chibumy' || userId === 'lkj83' || userId === 'haein' || userId === 'bell1' || userId === 'mila' || userId === 'won21kr') {
+    if (userId === 'lih7912' || userId === 'chibumy' || userId === 'lkj83' || userId === 'haein' || userId === 'bell1' || userId === 'mila' || userId === 'won21kr' || userId === 'brandon') {
         $('#btnAuth').css('display', 'block');
         $('#btnTrLog').css('display', 'block');
         return;
